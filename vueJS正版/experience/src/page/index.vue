1046282216ceabf53b1fba01749871d60d910e57
lj60 <lory.y.jiang@newegg.com>
2018-01-26 17:14:58 +0800

    1

---
+++ vueJS正版/experience/src/page/index.vue
@@ -0,0 +1,21 @@
+<template>
+  <div>index page
+    <myContent></myContent>
+  </div>
+</template>
+
+<script>
+import myContent from './content'
+
+export default {
+  components: {
+    myContent
+  },
+  created () {
+    this.$api.get('topics', null, r => {
+      console.log(r)
+    })
+  }
+}
+
+</script>
